0a7b0fcd94b8aa945be47fe353527c9c1b36a834
Ad31 Sh <68187289+ad31sh@users.noreply.github.com>
2024-04-23 12:54:32 +0300

    typescript working good

---
+++ src/index.js
@@ -0,0 +1,6 @@
+var btn1 = document.getElementById("clk-btn");
+btn1 === null || btn1 === void 0 ? void 0 : btn1.addEventListener("click", function () {
+    if (btn1) {
+        btn1.innerHTML = "Hello World";
+    }
+});
