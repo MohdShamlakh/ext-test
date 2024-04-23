171f956d3027bde5f6d471fb154ea3aa1372b418
Mohammad Shamlakh <167781724+MohdShamlakh@users.noreply.github.com>
2024-04-23 16:35:03 +0300

    New Branch Test!

--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
+var container = document.getElementsByClassName('container');
 var btn1 = document.getElementById("clk-btn");
-btn1 === null || btn1 === void 0 ? void 0 : btn1.addEventListener("click", function () {
+var i = 0;
+var randomList = ['You clicked me!', 'Oh, that hurts', 'STOP!', 'Yamete kudasai', 'Bro PLease!'];
+btn1 === null || btn1 === void 0 ? void 0 : btn1.addEventListener("click", function (event) {
     if (btn1) {
-        btn1.innerHTML = "Hello World";
+        if (i = 0)
+            btn1.innerHTML = 'Hello, World!';
+        else
+            btn1.innerHTML = randomList[Math.floor(Math.random() * 1000) % randomList.length];
     }
+    i++;
+    const caption = document.createElement('p');
+    const textnode = document.createTextNode(`You Clicked Me ${i} time${i > 1 ? 's' : ''}`);
+    caption.appendChild(textnode);
+    // Hola
+
+
+
+
+
+
+    document.getElementById("container").append(caption);
 });
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-let btn1: HTMLElement | null = document.getElementById("clk-btn");
+const btn1: HTMLElement | null = document.getElementById("clk-btn");
+const randomList: string[] = ['You clicked me!', 'Oh, that hurts', 'STOP!', 'Yamete kudasai', 'Bro PLease!'];
 
-btn1?.addEventListener("click", () =>
-{
+btn1?.addEventListener("click", function (event: Event) {
     if (btn1) {
-        btn1.innerHTML = "Hello World";
+        if (btn1.innerHTML == 'Click Me')
+            btn1.innerHTML = 'Hello, World!';
+        else
+            btn1.innerHTML = randomList[Math.floor(Math.random() * 1000) % randomList.length];
     }
 });
