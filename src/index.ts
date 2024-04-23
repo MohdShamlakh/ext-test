385546bdf5cdca881cbc4a3abf6baaaaf08a6512
Mohammad Shamlakh <167781724+MohdShamlakh@users.noreply.github.com>
2024-04-23 12:21:25 +0300

    Add TypeScript to the Project

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+let btn1: HTMLElement | null = document.getElementById("clk-btn");
+
+btn1?.addEventListener("click", () =>
+{
+    if (btn1) {
+        btn1.innerHTML = "Hello World";
+    }
+});
--- src/main.js
+++
@@ -1,7 +0,0 @@
-let container = document.getElementsByClassName("container");
-
-let btn = document.getElementById("clk-btn");
-
-btn.addEventListener("click", () => {
-  btn.innerHTML = "Hello World";
-});
